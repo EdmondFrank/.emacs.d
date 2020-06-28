9ffb2dc76dbd061a5cbafe17a205405b8fda68e5
EdmondFrank <EdmondFrank@hotmail.com>
2020-06-28 09:39:14 +0800

    feat(sidebar): added imenu-list enhance sidebar

--- lisp/init-sidebar.el
+++ lisp/init-sidebar.el
@@ -2,15 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 (require-package 'treemacs)
+(require-package 'imenu-list)
 
 ;; Add global keybindings
+
+;; treemacs
 (global-set-key (kbd "C-x C-d") 'treemacs)
 
+;; imenu-list
+(global-set-key (kbd "C-'") 'imenu-list-smart-toggle)
+
 ;; customize settings
+
+;; treemacs
 (setq treemacs-follow-mode t)
 (setq treemacs-filewatch-mode t)
 (setq treemacs-show-cursor t)
 (setq treemacs-width 44)
+
+;; imenu-list
+(setq imenu-list-auto-resize t)
 
 (provide 'init-sidebar)
 ;;; init-sidebar.el ends here
