e156510f51594f17825abd782beb03f133e9665c
EdmondFrank <EdmondFrank@hotmail.com>
2021-01-27 10:06:48 +0800

    feat(w3m): w3m support org inherit view

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -4,5 +4,7 @@
 (require-package 'nov)
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (require-package 'pdf-tools)
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/private/inherit-org"))
+(require 'inherit-org)
 (provide 'init-reader)
 ;;; init-reader.el ends here
