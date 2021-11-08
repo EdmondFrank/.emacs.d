7f01ba87044ab94483d0c74b1047b11376aa6778
EdmondFrank <EdmondFrank@hotmail.com>
2021-11-08 09:42:15 +0800

    alter display fill column indicator column 120

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -101,6 +101,7 @@
 (when (boundp 'display-fill-column-indicator)
   (setq-default indicate-buffer-boundaries 'left)
   (setq-default display-fill-column-indicator-character ?\u250a)
+  (setq-default display-fill-column-indicator-column 120)
   (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode))
 
 
