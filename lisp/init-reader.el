7a018faf1631af37384861fe2d6cd1ea99e69905
EdmondFrank <EdmondFrank@hotmail.com>
2022-03-01 09:15:05 +0800

    feat: support calibredb plugin

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -13,6 +13,12 @@
        :picker (gts-prompt-picker)
        :engines (list (gts-google-engine) (gts-google-rpc-engine))
        :render (gts-buffer-render)))
+(use-package calibredb
+  :defer t
+  :config
+  (setq calibredb-root-dir "~/Documents/CalibreLibrary")
+  (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
+  (setq calibredb-library-alist '(("~/Documents/CalibreLibrary"))))
 (require-package 'pdf-tools)
 (provide 'init-reader)
 ;;; init-reader.el ends here
