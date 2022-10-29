b143edb1addb81c9ffc3adbd6c31c52adc06fc55
EdmondFrank <EdmondFrank@hotmail.com>
2022-10-29 16:41:41 +0800

    Upgrade kubel && alter go-translate

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -3,14 +3,17 @@
 ;;; Code:
 (require-package 'nov)
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-(add-to-list 'load-path (expand-file-name "go-translate" user-emacs-directory))
+
+(add-to-list 'load-path (expand-file-name "site-lisp/go-translate" user-emacs-directory))
 (require 'go-translate)
+
 (setq gts-translate-list '(("en" "zh") ("zh" "en")))
 (setq gts-default-translator
       (gts-translator
        :picker (gts-prompt-picker)
        :engines (list (gts-google-engine) (gts-google-rpc-engine))
        :render (gts-buffer-render)))
+
 (use-package calibredb
   :defer t
   :config
