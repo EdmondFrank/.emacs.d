8cd1387f5ae7f584277974360f3b16fd91c52f67
Edmondfrank <EdmondFrank@hotmail.com>
2024-11-25 14:44:57 +0800

    Added aider for emacs ai development.
    Signed-off-by: Edmondfrank <EdmondFrank@hotmail.com>

--- lisp/init-gemini.el
+++ lisp/init-gemini.el
@@ -3,7 +3,9 @@
 ;;; Code:
 
 (add-to-list 'load-path (expand-file-name "site-lisp/Gemini.el" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "site-lisp/aider.el" user-emacs-directory))
 
+(require 'aider)
 (require 'gemini)
 
 (setq gemini-api-token "your token")
@@ -11,6 +13,16 @@
 (setq gemini-http-proxy "http://localhost:1081")
 
 (gemini-start-process)
+
+;; (setenv "HTTPS_PROXY" "http://localhost:1081")
+
+(setq aider-args '("--no-auto-commits" "--model" "openai/Qwen/Qwen2.5-Coder-7B-Instruct"))
+(setenv "OPENAI_API_BASE" "https://api.siliconflow.cn/v1/")
+(setenv "OPENAI_API_KEY" "access-token")
+
+
+
+(global-set-key (kbd "C-c q") 'aider-transient-menu)
 
 (provide 'init-gemini)
 ;;; init-gemini.el ends here
