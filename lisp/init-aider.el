95791aa14d33fb7d2072084e5e1ae9fef442d8d5
edmondfrank <edmondfrank@hotmail.com>
2025-11-05 17:58:50 +0800

    Update aider, exhub, and emacs-application-framework modules

--- lisp/init-aider.el
+++ lisp/init-aider.el
@@ -5,23 +5,31 @@
 (add-to-list 'load-path (expand-file-name "site-lisp/aider.el" user-emacs-directory))
 
 (require 'aider)
-(setq aider-program (expand-file-name ".local/bin/aider" (getenv "HOME")))
+(setq aider-program (expand-file-name "miniconda3/bin/aider-ce" (getenv "HOME")))
 
 (setenv "OPENAI_API_BASE" "http://localhost:9069/openai/v1")
 (setenv "OPENAI_API_KEY" "edmondfrank")
 
-(setq aider-args '("--no-auto-commits" "--architect" "--model" "openai/deepseek-v3_1" "--editor-model" "openai/qwen3-coder-480b-a35b-instruct" "--editor-edit-format" "editor-diff" "--map-tokens" "0"))
+(setq aider-args '("--no-auto-commits" "--yes" "--architect" "--model" "openai/deepseek-v3_1-terminus" "--editor-model" "openai/kimi-k2-instruct" "--editor-edit-format" "editor-diff" "--map-tokens" "0"))
 
 (setq aider-popular-models '("openai/glm-4_5"
+                             "openai/glm-4.6"
                              "openai/glm-4_5v"
                              "openai/gemini-2.5-pro"
                              "openai/deepseek-r1"
                              "openai/deepseek-v3"
                              "openai/deepseek-v3_1"
+                             "openai/deepseek-v3.2-exp"
+                             "openai/deepseek-v3_1-terminus"
                              "openai/gpt-oss-120b"
                              "openai/qwen3-235b-a22b"
                              "openai/qwen3-235b-a22b-instruct-2507"
+                             "openai/qwen3-next-80b-a3b-instruct"
                              "openai/qwen3-coder-480b-a35b-instruct"
+                             "openai/tngtech/deepseek-r1t2-chimera:free"
+                             "openai/minimax-m2"
+                             "openai/minimax-m2-preview"
+                             "openai/minimax/minimax-m2:free"
                              "openai/kimi-k2-instruct"
                              "mistral/mistral-large-latest"
                              "codestral/codestral-latest"
--- lisp/init-exhub.el
+++ lisp/init-exhub.el
@@ -24,8 +24,8 @@
   :config
   (require 'gptel-integrations)
   (setq
-   gptel-model 'qwen3-235b-a22b-instruct-2507
-   gptel-backend (gptel-make-openai "Cursor api" ;Any name you want
+   gptel-model 'qwen3-next-80b-a3b-instruct
+   gptel-backend (gptel-make-openai "Exhub api" ;Any name you want
                    :host "127.0.0.1:9069"
                    :endpoint "/openai/v1/chat/completions"
                    :stream t            ;for streaming responses
@@ -46,14 +46,14 @@
                               :mime-types ("image/png" "image/jpeg" "image/webp" "image/heic" "image/heif"
                                            "application/pdf" "text/plain" "text/csv" "text/html")
                               :context-window 64000)
-                             qwen3-235b-a22b-instruct-2507 qwen3-coder-480b-a35b-instruct kimi-k2-instruct deepseek-v3_1 glm-4_5 gemini-2.5-pro qwen3-235b-a22b cursor/claude-3-5-sonnet-200k cursor/o1-mini cursor/gemini-2.5-pro-exp-03-25 cursor/grok-3-mini cursor/deepseek-v3-1 cursor/gpt-4o-mini cursor/gpt-4.1 cursor/gpt-4o cursor/claude-3.7-sonnet cursor/claude-3.5-sonnet cursor/deepseek-r1))))
+                             tngtech/deepseek-r1t2-chimera:free minimax-m2 minimax-m2-preview qwen3-235b-a22b-instruct-2507 qwen3-coder-480b-a35b-instruct kimi-k2-instruct deepseek-v3_1 glm-4_5 glm-4.6 deepseek-v3.2-exp deepseek-v3_1-terminus gemini-2.5-pro qwen3-next-80b-a3b-instruct qwen3-235b-a22b))))
 
 (use-package mcp
   :load-path (lambda () (expand-file-name "site-lisp/mcp.el" user-emacs-directory))
   :after gptel
   :custom (mcp-hub-servers
            `(("probe" . (:command "bunx" :args ("-y" "@buger/probe-mcp")))
-             ("editor" . (:command "bunx" :args ("-y" "mcp-server-text-editor")) )
+             ("commander" . (:command "bunx" :args ("-y" "@wonderwhy-er/desktop-commander")))
              ("web-scout" . (
                              :command "bunx"
                              :args ("-y" "@pinkpixel/web-scout-mcp")
